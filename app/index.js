598f3ed1c0529bd1a0db0acf2057fafe74313c4e
mathmout <mathilde.mareite@gmail.com>
2017-11-08 00:59:18 +0100

    Created a brushable graph with axes

--- app/index.js
+++ app/index.js
@@ -1,4 +1,3 @@
-
 //Width and height
 var width = 800;
 var height = 500;
@@ -7,51 +6,58 @@ var padding = 50;
 //Define map projection
 var projection = d3.geoMercator()
                     .translate([width/2, height/2])
-                    .scale([100]);
+                    .scale([120]);
 
 //Define default path generator
 var path = d3.geoPath()
             .projection(projection);
 
-//Define quantize scale to sort data values into buckets of color
-var color = d3.scaleQuantize()
-                .range(["rgb(237,248,233)", "rgb(186,228,179)", "rgb(116,196,118)", "rgb(49,163,84)", "rgb(0,109,44)"]);
-
-//Create SVG element
-var svg = d3.select("body")
+//Create SVG element : map
+var map = d3.select("body")
             .append("svg")
             .attr("width", width)
             .attr("height", height);
+
+//Create SVG element : graph
+var graph = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height/3);
+
 
 
 //Load in GeoJSON data
 d3.json("/world.geo.json-master/countries.geo.json", function(json) {
 
     //Bind data and create one path per GeoJSON feature
-    svg.selectAll("path")
-       .data(json.features)
-       .enter()
-       .append("path")
-       .attr("d", path);
+    map.selectAll("path")
+        .data(json.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("class", "map");
+        
     
-    //Load in cities data
+    //Load airplane crashes data
     d3.csv("/data/aircrashes1.csv", function(data) {
         
-         // Scale
-        var date_min = new Date( d3.min(data, d => d["Date"]) , 1, 1);
-        var date_max = new Date( d3.max(data, d => d["Date"]), 1, 1);
+         // Define scales
+        var date_min = new Date( d3.min(data, d => d["Date"]));
+        var date_max = new Date( d3.max(data, d => d["Date"]));
         date_max.setFullYear(date_max.getFullYear()+1);
         var timeScale = d3.scaleLinear()
                             .domain( [date_min, date_max ])
                             .range([padding, width - padding]);
 
-        var fatalities_min = d3.min(data, d => d["Fatalities"]);
+       
         var fatalities_max = d3.max(data, d => d["Fatalities"]);
         var fatalitiesScale = d3.scaleLinear()
-                            .domain( [fatalities_min, fatalities_max ])
-                            .range([0.5, 3.5]);
-
-        svg.selectAll("circle")
+                            .domain( [ 0, fatalities_max ])
+                            .range([ height/3 - padding, 0 ]);
+        
+         
+        // Show crashes on the map
+        map.selectAll("circle")
            .data(data)
            .enter()
            .append("circle")
@@ -61,13 +67,46 @@ d3.json("/world.geo.json-master/countries.geo.json", function(json) {
            .attr("cy", function(d) {
                return projection([d.lng, d.lat])[1];
            })
-           .attr("r", function(d) {
-               return fatalitiesScale( d.Fatalities );
+           .attr("r", function(d) { 
+               return 0.5 + fatalitiesScale.invert( fatalitiesScale( d.Fatalities) ) / 50;
            })
            .style("fill", "yellow")
            .style("opacity", 0.75);
+        
+        
+        // Draw axis
+        const xAxis = d3.axisBottom( timeScale )
+				        .tickFormat( d3.timeFormat("%Y") );				
+        graph.append("g")
+            .attr("class", "axis axis--grid")
+            .attr("transform", "translate(0," + (height/3 - padding) + ")")
+            .call(xAxis)
+            .selectAll("text") // Rotate labels 
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-45)")
+            .selectAll(".tick")
+            .classed("tick--minor", function(d) { return new Date( d["Date"] ); });
+
+        const yAxis = d3.axisLeft( fatalitiesScale )
+				        .ticks( 5 );
+        graph.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + padding + ",0)")
+            .call(yAxis);
+       
+        graph.append("g")
+				.attr("class", "brush")
+				.call(d3.brushX()
+				.extent([[0, 0], [width, height]])
+				.on("end", brushended));
+
 
     });
 
+    function brushended() {
+        // TODO when brush
+    }
 
 });
