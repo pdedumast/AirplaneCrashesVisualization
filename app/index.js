96da9a7546da701d9bdec089875929677177fd93
Thibaut Chamard <tchamard@gmail.com>
2017-11-14 14:24:04 +0100

    Refractored the code.

--- app/index.js
+++ app/index.js
@@ -3,8 +3,11 @@ const width = 1000;
 const height = 500;
 const padding = 50;
 
-console.log("Mathmout est dans sa douche !!");
-let tooltip
+let map;
+let country;
+let tooltip;
+
+
 
 /* *************************************************** */
 // Functions to manage zooming and dragging on the map
@@ -31,6 +34,8 @@ function stopped() {
 }
 /* **************************************************** */
 
+map = new Map();
+map.setUp();
 
 //Define map projection
 let projection = d3.geoMercator()
@@ -44,19 +49,7 @@ projection = d3.geoNaturalEarth1();
 let path = d3.geoPath()
             .projection(projection);
 
-//Create SVG element : map
-let map = d3.select("body")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .on("click", stopped, true);
-map.append("rect")
-    .attr("class", "background")
-    .attr("width", width)
-    .attr("height", height)
-    .on("click", reset);
-let country = map.append("g");
-map.call(zoom);
+
 
 
 //Create SVG element : graph
@@ -70,40 +63,46 @@ let timeScale = d3.scaleLinear().range([padding, width - padding]);
 let fatalitiesScale = d3.scaleLinear().range([ height/3 - padding, 0 ]);
 
 
-//Load in GeoJSON data
-d3.json("/world.geo.json-master/countries.geo.json", function(json) {
-
-    //Bind data and create one path per GeoJSON feature
-    country.selectAll("path")
-        .data(json.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .style("fill", "#fcda94")
-        .style('stroke', '#fcbf94')
-        .style('stroke-width', '0.4')
-        .on("click", reset);
-
-    //Load airplane crashes data
-    d3.csv("/data/aircrashes1.csv", function(data) {
-
-         // Define scales DOMAIN
-        const date_min = new Date( d3.min(data, d => d["Date"]));
-        const date_max = new Date( d3.max(data, d => d["Date"]));
-        date_max.setFullYear(date_max.getFullYear()+1);
-        timeScale.domain( [date_min, date_max]);
-
-        const fatalities_max = d3.max(data, d => d["Fatalities"]);
-        fatalitiesScale.domain( [ 0, fatalities_max ]);
-
-
-        // Define tooltip for crashes market
-        tooltip = d3.select("body").append("div").attr("class", "toolTip");
-
-
-
-        // Show crashes on the map
-        map.selectAll("circle")
+function Map(){
+    // Attributes
+    this.map
+    this.country
+    
+    // Functions
+    this.setUp = function(){
+        
+        this.map = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .on("click", stopped, true);
+        
+        this.map.append("rect")
+            .attr("class", "background")
+            .attr("width", width)
+            .attr("height", height)
+            .on("click", reset);
+        
+        this.map.call(zoom);
+        
+        this.country = this.map.append("g");
+        
+        console.log("inside map");
+    }
+    this.drawMap = function(json){
+        this.country.selectAll("path")
+                .data(json.features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .style("fill", "#fcda94")
+                .style('stroke', '#fcbf94')
+                .style('stroke-width', '0.4')
+                .on("click", reset);
+    }
+    
+    this.drawCrashes = function(data){
+        this.map.selectAll("circle")
             .data(data)
             .enter()
             .append("circle")
@@ -129,64 +128,113 @@ d3.json("/world.geo.json-master/countries.geo.json", function(json) {
                         + "Fatalities : " + parseInt(d.Fatalities) + "/" + parseInt(d.Aboard))
                     .on("click", function(d){
                         tooltip.style("display", "none");
-                    });
+                });
         })
-
-
-        // Draw axis
-        const xAxis = d3.axisBottom( timeScale )
-				        .tickFormat( d3.timeFormat("%Y") );
-        graph.append("g")
-            .attr("class", "axis axis--grid")
-            .attr("transform", "translate(0," + (height/3 - padding) + ")")
-            .call(xAxis)
-            .selectAll("text") // Rotate labels
-            .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
-            .attr("transform", "rotate(-45)")
-            .selectAll(".tick")
-            .classed("tick--minor", function(d) { return new Date( d["Date"] ); });
-
-        const yAxis = d3.axisLeft( fatalitiesScale )
-				        .ticks( 5 );
-        graph.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(" + padding + ",0)")
-            .call(yAxis);
-
-        graph.append("g")
-				.attr("class", "brush")
-				.call(d3.brushX()
-				.extent([[padding, 0], [width - padding, height/3 + padding]])
-				.on("end", brushended));
-
-        // *** Filter by year ***
-        let year_min = 1920, year_max = 1925;
-
-        // Create list of kept years
-        const list_years = (min = 1900, max = 2000) => [...Array(max - min + 1)].map((x,i) => min + i);
-        my_list_years = list_years(year_min, year_max)
-
-        // Keep only selected years
-        const data_selectedYears = data.filter((x) =>  new Date(x.Date).getFullYear() >= year_min && new Date(x.Date).getFullYear() <= year_max)
-
-        // Sum fatalities over the years
-        const nested_data_year =  data_selectedYears.map
-        console.log(new Date(data_selectedYears[0].Date));
-
-        console.log("après data_by_year");
-
-    });
-
-    function brushended() {
-      // if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
-      // Get year range selected
-      var s = d3.event.selection || timeScale.range();  // if no brush, we take full range
-      console.log(s.map(x => new Date(timeScale.invert(x)).getFullYear()));
     }
+}
+
+
+/* **************************************************** */
+function drawMap(json){
+    country.selectAll("path")
+        .data(json.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .style("fill", "#fcda94")
+        .style('stroke', '#fcbf94')
+        .style('stroke-width', '0.4')
+        .on("click", reset);
+}
+
+function drawCrashes(data){
+    
+}
+
+//Load in GeoJSON data
+d3.json("/world.geo.json-master/countries.geo.json", function(error, json) {
+    if (error) throw error;
+    //Bind data and create one path per GeoJSON feature
+    map.drawMap(json);
+    
+})
+
+
+//Load airplane crashes data
+d3.csv("/data/aircrashes1.csv", function(error, data) {
+    if (error) throw error;
+    // Show crashes on the map
+    map.drawCrashes(data);
+
+
+     // Define scales DOMAIN
+    const date_min = new Date( d3.min(data, d => d["Date"]));
+    const date_max = new Date( d3.max(data, d => d["Date"]));
+    date_max.setFullYear(date_max.getFullYear()+1);
+    timeScale.domain( [date_min, date_max]);
+
+    const fatalities_max = d3.max(data, d => d["Fatalities"]);
+    fatalitiesScale.domain( [ 0, fatalities_max ]);
+
+
+    // Define tooltip for crash markers
+    tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
+
+    // Draw axis
+    const xAxis = d3.axisBottom( timeScale )
+                    .tickFormat( d3.timeFormat("%Y") );
+    graph.append("g")
+        .attr("class", "axis axis--grid")
+        .attr("transform", "translate(0," + (height/3 - padding) + ")")
+        .call(xAxis)
+        .selectAll("text") // Rotate labels
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-45)")
+        .selectAll(".tick")
+        .classed("tick--minor", function(d) { return new Date( d["Date"] ); });
+
+    const yAxis = d3.axisLeft( fatalitiesScale )
+                    .ticks( 5 );
+    graph.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + padding + ",0)")
+        .call(yAxis);
+
+    graph.append("g")
+            .attr("class", "brush")
+            .call(d3.brushX()
+            .extent([[padding, 0], [width - padding, height/3 + padding]])
+            .on("end", brushended));
+
+    // Filter by year
+    console.log("avant data_by_year");
+
+    // Create list of kept years
+    const list_years = (min = 1900, max = 2000) => [...Array(max - min + 1)].map((x,i) => min + i);
+    my_list_years = list_years(2010, 2015)
+    console.log(my_list_years);
+
+    // Keep only selected years
+    const nested_data = data.filter((x) =>  new Date(x.Date).getFullYear() >= year_min && new Date(x.Date).getFullYear() <= year_max)
+
+    // Sum fatalities over the years
+    const nested_data_year =  nested_data.map
+    console.log(new Date(nested_data[0].Date));
+
+    console.log("après data_by_year");
 
 });
+
+function brushended() {
+  // if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+  // Get year range selected
+  var s = d3.event.selection || timeScale.range();  // if no brush, we take full range
+  console.log(s.map(x => new Date(timeScale.invert(x)).getFullYear()));
+}
+
 
 
 
