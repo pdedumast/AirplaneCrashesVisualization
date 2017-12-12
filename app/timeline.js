63b8dcbdd981257c9a834741230cc5bd839ec740
mathmout <mathilde.mareite@gmail.com>
2017-12-12 19:40:01 +0100

    ENH : Invert x and y axis

--- app/timeline.js
+++ app/timeline.js
@@ -4,28 +4,21 @@
 d3.json(pathname + "/data/timeline.json", function(error,dataset) {
     if (error) throw error;
 
-    console.log("data timeline");
-    console.log(dataset);
-
     graph.selectAll("text")
                    .data(dataset)
                    .enter()
                    .append("text")
                    .text(function(d) {
-               console.log("lamaaaa");
-                console.log("Date = " + d.date);
-                console.log("Event = " + d.event);
-                        return "Allo";
+                        return d.event;
                    })
                    .attr("x", function(d) {
-               console.log("x = " + timeScale(d.date));
-                        return timeScale(3);
+                        return 150;
                    })
                    .attr("y", function(d) {
-               console.log("y = " + 0);
-                        return 0;
+                        return timeScale( d.date ) ;
                    })
                    .attr("font-family", "sans-serif")
                    .attr("font-size", "11px")
-                   .attr("fill", "red");
+                   .attr("fill", "white")
+                    .attr("text-anchor", "middle");
 })
